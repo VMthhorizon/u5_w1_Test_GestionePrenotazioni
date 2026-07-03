886ad73733329024f931fb47dccb6503894f644b
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-03 14:29:16 +0200

    ok

---
+++ u5_w1_Test_Prenotazioni/src/main/java/vincenzomola/u5_w1_Test_Prenotazioni/U5W1TestPrenotazioniApplication.java
@@ -0,0 +1,16 @@
+package vincenzomola.u5_w1_Test_Prenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1TestPrenotazioniApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(U5W1TestPrenotazioniApplication.class, args);
+
+		System.out.println("COMPLETATO");;
+	}
+
+}
---
+++ u5_w1_Test_Prenotazioni/src/test/java/vincenzomola/u5_w1_Test_Prenotazioni/U5W1TestPrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5_w1_Test_Prenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1TestPrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
